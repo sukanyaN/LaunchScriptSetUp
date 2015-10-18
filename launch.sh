7be1d92ae646504d4a01f353085b8a7634a1fab9
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-18 10:00:08 -0500

    wrongly removed the file from git. adding again

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://EnvSetUp/install-env.sh --debug
+
+
+
